ad5bbfbd0b9d3d73e5245f647908cbb82f56e7b3
Anders Pearson <anders@columbia.edu>
2016-05-01 14:42:11 +0100

    use named URL

--- main/tests.py
+++ main/tests.py
@@ -1,11 +1,13 @@
+import json
+
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils.encoding import smart_str
-import json
 
 
 class BasicTest(TestCase):
     def test_returns_json(self):
-        r = self.client.get("/debug/vars")
+        r = self.client.get(reverse('expvar'))
         self.assertEqual(r.status_code, 200)
         self.assertEqual(r['Content-Type'], 'application/json')
         json.loads(smart_str(r.content))
@@ -13,6 +15,6 @@ class BasicTest(TestCase):
 
 class CmdlineTest(TestCase):
     def test_cmdline(self):
-        r = self.client.get("/debug/vars")
+        r = self.client.get(reverse('expvar'))
         d = json.loads(smart_str(r.content))
         self.assertIn('cmdline', d)
--- testapp/urls.py
+++ testapp/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from expvar.views import ExpVarView
 
 urlpatterns = [
-    url('^debug/vars$', ExpVarView.as_view()),
+    url('^debug/vars$', ExpVarView.as_view(), name='expvar'),
 ]
