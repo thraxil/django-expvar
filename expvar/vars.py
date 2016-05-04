53a3d24e89230524f6c4774fbaf0a0452f0da2d8
Anders Pearson <anders@columbia.edu>
2016-05-04 19:00:40 +0100

    that shouldn't be a dict

--- expvar/vars.py
+++ expvar/vars.py
@@ -8,7 +8,7 @@ class CmdlineExpVar(ExpVar):
     name = "cmdline"
 
     def value(self):
-        return dict(cmdline=[sys.executable] + sys.argv)
+        return [sys.executable] + sys.argv
 
 
 class MemoryExpVar(ExpVar):
